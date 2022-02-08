86317cd8a1f932be43bfcbe3ce4a974df67b2f68
Julian Beitinger <49885564+AnimaComedenti@users.noreply.github.com>
2022-02-08 14:33:06 +0100

    Ar-Basics

---
+++ Assets/Player/Seeker/ARPlacementIndicator.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class ARPlacementIndicator : MonoBehaviour
+{
+
+    public GameObject levelToSpawn;
+    public GameObject placementIdicator;
+    //could be Private
+    public GameObject[] gameObjectsToSpawnInLevel;
+
+    private GameObject spawnedGameObject;
+    private Pose placementPosition;
+    private ARRaycastManager aRRaycastManager;
+    private bool isLevelPlaced = false;
+    private bool isPlacementValid = false;
+
+    void Start()
+    {
+        aRRaycastManager = FindObjectOfType<ARRaycastManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(spawnedGameObject == null && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            PlaceGameObject();
+        }
+        UpdatePlacementPosition();
+        UpdatePlacementIndicator();
+    }
+
+    private void UpdatePlacementIndicator()
+    {
+        if(spawnedGameObject == null && isPlacementValid)
+        {
+            placementIdicator.SetActive(true);
+            placementIdicator.transform.SetPositionAndRotation(placementPosition.position, placementPosition.rotation);
+        }
+        else
+        {
+            placementIdicator.SetActive(false);
+        }
+    }
+
+    private void UpdatePlacementPosition()
+    {
+        var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.3f));
+        var hits = new List<ARRaycastHit>();
+        aRRaycastManager.Raycast(screenCenter, hits, TrackableType.Planes);
+
+        isPlacementValid = hits.Count > 0;
+        if (isPlacementValid)
+        {
+            placementPosition = hits[0].pose;
+        }
+    }
+
+    private void PlaceGameObject()
+    {
+        if(isPlacementValid && !isLevelPlaced)
+        {
+            //Place LevelObject
+            spawnedGameObject = Instantiate(levelToSpawn, placementPosition.position, placementPosition.rotation);
+            //isLevelPlaced = true;
+        }
+        else if (isPlacementValid)
+        {
+            //Buildsystem
+            spawnedGameObject = Instantiate(levelToSpawn, placementPosition.position, placementPosition.rotation);
+        }
+    }
+
+    //Getter and Setters
+}
